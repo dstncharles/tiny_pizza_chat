5e98a95f86fd908e7c47d8ef2539c25c8de7a73c
Dustin Morando <dustinmorando@me.com>
2015-01-31 14:12:04 -0500

    linked moment like a fucking moron

--- main.js
+++ main.js
@@ -7,23 +7,23 @@
 
 
 
-    var chatTemplate = _.template($('[data-template-name=chat]').text());
-    var $chatLogUl = $(".tinyPizzaChatLog");
-    $.ajax(pizzaUrl).done(function(chatArray) {
-      _.each(chatArray, function(chatObject) {
-        _.defaults(chatObject, {
-          message: "",
-          username: "",
-          createdAt:""
-        });
-        $chatLogUl.append(chatTemplate(chatObject));
+
+  //////chat template block
+  var chatTemplate = _.template($('[data-template-name=chat]').text());
+  var $chatLogUl = $(".tinyPizzaChatLog");
+  $.ajax(pizzaUrl).done(function(chatArray) {
+    _.each(chatArray, function(chatObject) {
+      _.defaults(chatObject, {
+        message: "",
+        username: "",
+        createdAt: ""
       });
+      $chatLogUl.append(chatTemplate(chatObject));
     });
+  });
 
 
   //-----------------
-
-
 
   $('form').on('submit', function(event) {
     event.preventDefault();
@@ -35,60 +35,47 @@
       data: {
         message: messageOutPut,
         username: userLogin,
-        createdAt: "test"
+        createdAt: Date.now()
       }
     });
-});
-
-
-
-////refresh page
-
-$('form').on('submit', function(event){
-  userLogin = $(".login").val();
-  console.log(userLogin);
-$( ".login_page"  ).css( "display" , "none" );
-$( ".chatroom"  ).css( "display" , "block" );
-setInterval(function(){
-updateChat();
-}, 5000);
- });
-
-
-
-function updateChat(){
-  $.ajax(pizzaUrl).done(function(chatArray) {
-    _.each(chatArray, function (chatObject) {
-      _.defaults(chatObject, {
-        message:"",
-        username:"",
-        createdAt:""
-      });
-    });
-    console.log(chatArray);
-    $(".tinyPizzaChatLog").empty();
-    _.each(chatArray, function(chatObject) {
-      _.defaults(chatObject, {
-        message: "",
-        username: "",
-        createdAt:""
-      });
-      $(".tinyPizzaChatLog").append(chatTemplate(chatObject));
-    });
   });
-}
-
-
-// //toggle class//
-// $( "div.login_page" ).toggleClass( "chatroom" ),
-//
-// if ( addOrRemove ) {
-//   $( "userLogin" ).addClass(  );
-// } else {
-//   $( "#foo" ).removeClass( className );
-// }
 
 
 
+////////////submit function and refresh page
+
+
+
+  $('form').on('submit', function(event) {
+    userLogin = $(".login").val();
+    console.log(userLogin);
+    $(".login_page").css("display", "none");
+    $(".chatroom").css("display", "block");
+    setInterval(function() {
+      updateChat();
+    }, 5000);
+  });
+
+  function updateChat() {
+    $.ajax(pizzaUrl).done(function(chatArray) {
+      _.each(chatArray, function(chatObject) {
+        _.defaults(chatObject, {
+          message: "",
+          username: "",
+          createdAt: ""
+        });
+      });
+      console.log(chatArray);
+      $(".tinyPizzaChatLog").empty();
+      _.each(chatArray, function(chatObject) {
+        _.defaults(chatObject, {
+          message: "",
+          username: "",
+          createdAt: ""
+        });
+        $(".tinyPizzaChatLog").append(chatTemplate(chatObject));
+      });
+    });
+  }
 
 })();
